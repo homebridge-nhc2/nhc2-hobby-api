6ea8861a811724e4597d4da53a0ecb4dab58cbaa
Wouter Van Vlaenderen <wvanvlaenderen@gmail.com>
2020-10-07 13:26:28 +0200

    Added sample for logging all devices

---
+++ samples/log-all-devices.ts
@@ -0,0 +1,19 @@
+import {NHC2} from '../src/NHC2';
+
+const nhc2 = new NHC2('mqtts://192.168.0.216', {
+    port: 8884,
+    clientId: 'clientId',
+    username: 'hobby',
+    password: 'password',
+    rejectUnauthorized: false,
+});
+
+(async () => {
+    await nhc2.subscribe();
+
+    const accessories = await nhc2.getAccessories();
+    accessories.forEach(accessory => {
+            console.log(accessory)
+        });
+})();
+
