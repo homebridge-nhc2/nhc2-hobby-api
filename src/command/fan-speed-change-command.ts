6fc6512196982fcc6aaccbf76f052143b8b35f3b
Wouter Van Vlaenderen <wvanvlaenderen@gmail.com>
2020-12-23 09:58:27 +0100

    Trying to fix a flaky build caused by the individual specs receiving events sent in other specs. Receiving events are now handled in the end.

--- src/command/fan-speed-change-command.ts
+++ src/command/fan-speed-change-command.ts
@@ -1,9 +1,9 @@
 import { FanSpeed } from '../event/FanSpeed';
 import { Method } from './method';
 
-export const STATUS_CHANGE_COMMAND_TOPIC = 'hobby/control/devices/cmd';
+export const FAN_SPEED_CHANGED_COMMAND_TOPIC = 'hobby/control/devices/cmd';
 export const FanSpeedChangeCommand = (uuid: string, speed: FanSpeed) => ({
-  topic: STATUS_CHANGE_COMMAND_TOPIC,
+  topic: FAN_SPEED_CHANGED_COMMAND_TOPIC,
   data: {
     Method: Method.DEVICES_CONTROL,
     Params: [
